54e33e1302ee2c553ed0a51efc18f3f6306e5240
Chylli <chylli.email@gmail.com>
2014-01-16 10:47:41 +0800

    add config examle of smthp email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,19 @@ Depot::Application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "domain.of.sender.net",
+  #  authentication: "plain",
+  #  user_name: "dave",
+  #  password: "secret",
+  #  enable_starttls_auto: true
+  #
+  #}
+
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
